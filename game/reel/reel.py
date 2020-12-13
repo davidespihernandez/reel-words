ebd403c24456d724f71edb173653288ea42688a7
David Espí Hernández <davidespihernandez@gmail.com>
2020-12-13 12:07:24 +0100

    Reel class + tests

---
+++ game/reel/reel.py
@@ -0,0 +1,44 @@
+import random
+from pathlib import Path
+from typing import Optional, List
+
+
+class Reel:
+    initial_index: int
+    letters: str
+
+    def __init__(self, letters: str, initial_index: Optional[int] = None):
+        if not letters or len(letters) <= 1:
+            raise ValueError("A reel needs at least 2 letters")
+        if initial_index is not None and (
+            initial_index >= len(letters) or initial_index < 0
+        ):
+            raise ValueError("Wrong initial index")
+        self.letters = letters
+        self.initial_index = (
+            initial_index
+            if initial_index is not None
+            else random.randint(0, len(letters) - 1)
+        )
+
+    def __next__(self):
+        return_value = self.current_value()
+        self.initial_index += 1
+        if self.initial_index >= len(self.letters):
+            self.initial_index = 0
+        return return_value
+
+    def current_value(self) -> str:
+        return self.letters[self.initial_index]
+
+    def __iter__(self):
+        return self
+
+    def __eq__(self, other: "Reel"):
+        return self.letters == other.letters
+
+    @staticmethod
+    def get_from_file(path: Path) -> List["Reel"]:
+        with path.open() as file:
+            file_contents = file.read().splitlines()
+        return [Reel(letters=line.replace(" ", "")) for line in file_contents]
---
+++ tests/test_reel.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from unittest import TestCase
+
+from game.reel.reel import Reel
+
+
+class TestReel(TestCase):
+    def test_get_from_file(self):
+        expected = [
+            Reel(letters="udxclae"),
+            Reel(letters="eyvpqyn"),
+        ]
+        path = Path(__file__).parent / "resources" / "reels.txt"
+        result = Reel.get_from_file(path)
+        self.assertEqual(len(result), len(expected))
+        self.assertEqual(result, expected)
+
+    def test_constructor_no_initial_index(self):
+        letters = "abcd"
+        reel = Reel(letters=letters)
+        self.assertEqual(reel.letters, letters)
+        self.assertTrue(reel.initial_index < len(letters))
+
+    def test_constructor_with_index(self):
+        letters = "abcd"
+        initial_index = 3
+        reel = Reel(letters=letters, initial_index=initial_index)
+        self.assertEqual(reel.letters, letters)
+        self.assertEqual(reel.initial_index, initial_index)
+
+    def test_constructor_wrong_index(self):
+        letters = "abcd"
+        with self.assertRaises(ValueError):
+            Reel(letters=letters, initial_index=-1)
+
+        with self.assertRaises(ValueError):
+            Reel(letters=letters, initial_index=len(letters))
+
+    def test_iterator(self):
+        letters = "abc"
+        reel = Reel(letters=letters, initial_index=1)
+        result = []
+        expected = ["b", "c", "a"]
+        for i in range(len(letters)):
+            result.append(reel.current_value())
+            next(reel)
+        self.assertEqual(result, expected)
